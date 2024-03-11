9cbad3fac57cd6204ad605537bac3bc6dd4b32b3
kadincool <kadinalone@gmail.com>
2024-03-10 22:10:29 -0600

    rotation and 3d flying mechanics

--- raycast/rayCast.js
+++ raycast/rayCast.js
@@ -17,6 +17,7 @@ const fShader = `
 precision mediump float;
 uniform vec2 screenSize;
 uniform vec3 camPos;
+uniform mat4 camRot;
 uniform int rayDist;
 
 //boolean decides wether to round up or down
@@ -85,6 +86,7 @@ bool checkTile(vec3 pos) {
 
 //raycast, returns hit (or fade out) position as XYZ and distance as W
 vec4 raycast(vec3 start, vec3 end) {
+  //make ray data
   vec3 offset = end - start;
   mat3 slopes = mat3(
     offset.x / offset.x, offset.y / offset.x, offset.z / offset.x,
@@ -100,7 +102,9 @@ vec4 raycast(vec3 start, vec3 end) {
   if (offset == vec3(0.0, 0.0, 0.0)) {
     return vec4(rayPos, 0);
   }
+  //basically a while true
   for (int i=0; i<32768; i++) {
+    //check current tile
     vec3 tileAt = vec3(
       floor(rayPos.x) - float(offset.x < 0.0 && fract(rayPos.x)==0.0),
       floor(rayPos.y) - float(offset.y < 0.0 && fract(rayPos.y)==0.0),
@@ -109,12 +113,14 @@ vec4 raycast(vec3 start, vec3 end) {
     if (checkTile(tileAt)) {
       return vec4(rayPos, distance(start, rayPos));
     }
+    //get offset and distance to nearest directiom
     vec3 off = vec3(
       ceilFloor(rayPos.x + sign(offset.x), offset.x < 0.0) - rayPos.x,
       ceilFloor(rayPos.y + sign(offset.y), offset.y < 0.0) - rayPos.y,
       ceilFloor(rayPos.z + sign(offset.z), offset.z < 0.0) - rayPos.z
     );
     vec3 dist = vec3(abs(off * travelDist));
+    //choose an axis
     int travelAxis = -1;
     float axisTravelDist = 1.0/0.0; //set to Infinity
     //find closest axis
@@ -130,6 +136,7 @@ vec4 raycast(vec3 start, vec3 end) {
       travelAxis = 2;
       axisTravelDist = dist.z;
     }
+    //travel along axis
     if (travelAxis == 0) {
       rayPos += slopes[0] * off.x;
     } else if (travelAxis == 1) {
@@ -137,6 +144,7 @@ vec4 raycast(vec3 start, vec3 end) {
     } else if (travelAxis == 2) {
       rayPos += slopes[2] * off.z;
     }
+    //if too far return 0 and max dist
     if (distance(start, rayPos) > float(rayDist)) {
       return vec4(vec3(0.0, 0.0, 0.0), rayDist);
     }
@@ -146,7 +154,9 @@ vec4 raycast(vec3 start, vec3 end) {
 
 void main() {
   vec2 uv = (gl_FragCoord.xy * 2.0 - screenSize) / screenSize.xx;
-  vec4 rCast = raycast(camPos, vec3(uv, 1.0)+camPos);
+  vec3 castDir = vec3(uv, 1.0);
+  castDir = (vec4(castDir, 1.0) * camRot).xyz;
+  vec4 rCast = raycast(camPos, castDir+camPos);
   //float perlin = perlin3d(vec3(gl_FragCoord.xy, 0.0), 0.0, 1.0, 5.0);
   gl_FragColor = vec4(1.0 - rCast.www/float(rayDist), 1.0);
   //gl_FragColor = vec4(perlin, perlin, perlin, 1.0);
@@ -174,19 +184,13 @@ console.log("Vertex Shader:", gl.getShaderInfoLog(vs));
 console.log("Fragment Shader:", gl.getShaderInfoLog(fs));
 console.log("Program:", gl.getProgramInfoLog(program));
 
-//get and set screenSize uniform
+//get uniforms
 var screenSize = gl.getUniformLocation(program, "screenSize");
-gl.uniform2f(screenSize, canvasgl.width, canvasgl.height);
-
-//get and set the camera position
 var cameraPosition = gl.getUniformLocation(program, "camPos");
-gl.uniform3f(cameraPosition, 0.0, 0.0, 0.0);
 var cameraRotation = gl.getUniformLocation(program, "camRot");
-gl.uniform3f(cameraRotation, 0.0, 0.0, 0.0);
 
 //get and set rayDist
 var rayDist = gl.getUniformLocation(program, "rayDist");
-gl.uniform1i(rayDist, 256);
 
 //make 2 triangles that fill up the screen
 var position = gl.getAttribLocation(program, "position"); //get point shader value
@@ -207,53 +211,80 @@ gl.enableVertexAttribArray(posBuffer); //enables the array
 //set clear color
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-//clear
-gl.clear(gl.COLOR_BUFFER_BIT);
-
-//drawPoints
-gl.drawArrays(gl.TRIANGLES, 0, 6);
-
-//canvas2d
-function pmod(a,b) {return (a%b+b)%b}
-function ceilFloor(number, ceil) {return (ceil ? Math.ceil(number) : Math.floor(number))}
-function flip(a, flip) {return (flip ? 1 - a : a)}
-
 var scale = 1;
 const castDist = 64;
+const moveSpeed = 0.2;
+const rotSpeed = 1;
+var keys = {};
 
-var camPos = {x: 0, y: 0, z: 0}
+var camPos = {x: 0, y: 0, z: 0};
+var camRot = {x: 0, y: 0, z: 0};
 
 function frame() {
+  let camMatrix = new DOMMatrix();
+  // camMatrix.rotateSelf(camRot.x, camRot.y, camRot.z);
+  camMatrix.rotateSelf(camRot.x, 0, 0);
+  camMatrix.rotateSelf(0, camRot.y, 0);
+  camMatrix.rotateSelf(0, 0, camRot.z);
+
+  //move
+  if (keys.KeyW) {
+    camPos.x += camMatrix.m13 * moveSpeed;
+    camPos.y += camMatrix.m23 * moveSpeed;
+    camPos.z += camMatrix.m33 * moveSpeed;
+  };
+  if (keys.KeyS) {
+    camPos.x -= camMatrix.m13 * moveSpeed;
+    camPos.y -= camMatrix.m23 * moveSpeed;
+    camPos.z -= camMatrix.m33 * moveSpeed;
+  };
+  if (keys.KeyD) {
+    camPos.x += camMatrix.m11 * moveSpeed;
+    camPos.y += camMatrix.m21 * moveSpeed;
+    camPos.z += camMatrix.m31 * moveSpeed;
+  };
+  if (keys.KeyA) {
+    camPos.x -= camMatrix.m11 * moveSpeed;
+    camPos.y -= camMatrix.m21 * moveSpeed;
+    camPos.z -= camMatrix.m31 * moveSpeed;
+  };
+  // if (keys.KeyE) {
+  //   camPos.x += camMatrix.m12 * moveSpeed;
+  //   camPos.y += camMatrix.m22 * moveSpeed;
+  //   camPos.z += camMatrix.m32 * moveSpeed;
+  // };
+  // if (keys.KeyQ) {
+  //   camPos.x -= camMatrix.m12 * moveSpeed;
+  //   camPos.y -= camMatrix.m22 * moveSpeed;
+  //   camPos.z -= camMatrix.m32 * moveSpeed;
+  // };
+  // if (keys.KeyD) camPos.x += 0.2;
+  // if (keys.KeyA) camPos.x -= 0.2;
+  if (keys.KeyE) camPos.y += 0.2;
+  if (keys.KeyQ) camPos.y -= 0.2;
+
+  if (keys.ArrowLeft) camRot.y += rotSpeed;
+  if (keys.ArrowRight) camRot.y -= rotSpeed;
+  if (keys.ArrowUp) camRot.x += rotSpeed;
+  if (keys.ArrowDown) camRot.x -= rotSpeed;
+
+  //render
   canvasgl.width = Math.ceil(window.innerWidth/scale);
   canvasgl.height = Math.ceil(window.innerHeight/scale);
   gl.uniform3f(cameraPosition, camPos.x, camPos.y, camPos.z);
+  gl.uniformMatrix4fv(cameraRotation, false, camMatrix.toFloat32Array());
   gl.uniform2f(screenSize, canvasgl.width, canvasgl.height);
   gl.uniform1i(rayDist, castDist);
   gl.clear(gl.COLOR_BUFFER_BIT);
   gl.drawArrays(gl.TRIANGLES, 0, 6);
+  requestAnimationFrame(frame);
 }
 frame();
+console.log(new DOMMatrix().rotateSelf(0, 45, 0));
 
 document.addEventListener("keydown", (e) => {
-  switch(e.code) {
-    case ("KeyW"):
-      camPos.z+=0.1;
-      break;
-    case("KeyS"):
-      camPos.z-=0.1;
-      break;
-    case("KeyD"):
-      camPos.x+=0.1;
-      break;
-    case("KeyA"):
-      camPos.x-=0.1;
-      break;
-    case("KeyE"):
-      camPos.y+=0.1;
-      break;
-    case("KeyQ"):
-      camPos.y-=0.1;
-      break;
-  }
-  frame()
+  keys[e.code] = true;
+});
+document.addEventListener("keyup", (e) => {
+  keys[e.code] = false;
 })
