7c8631203a2a70811498b2e84bff4077711f1af4
kadincool <kadinalone@gmail.com>
2024-03-11 08:09:12 -0600

    update rayCast.js

--- raycast/rayCast.js
+++ raycast/rayCast.js
@@ -241,7 +241,7 @@ const moveSpeed = 0.2;
 const rotSpeed = 1;
 var keys = {};
 
-var camPos = {x: 0, y: 0, z: 350};
+var camPos = {x: 0, y: 0, z: 350}; //github keeps refusing to update
 var camRot = {x: 0, y: 0, z: 0};
 
 function frame() {
